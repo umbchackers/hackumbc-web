71c1f31a7fcc23595fac8ac230b554422442f5ed
Mahad O <40477767+proobs@users.noreply.github.com>
2024-09-02 16:19:07 -0400

    add bucket support

--- src/app/api/submit/route.js
+++ src/app/api/submit/route.js
@@ -1,4 +1,16 @@
 import { NextResponse } from "next/server";
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
+
+const Bucket = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
+const s3 = new S3Client({
+  region: process.env.NEXT_PUBLIC_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 export async function POST(request) {
   try {
@@ -7,25 +19,24 @@ export async function POST(request) {
     const data = {
       major: "",
     };
+    let resumeResult;
 
-    formData.forEach((value, key) => {
+    for (let [key, value] of formData.entries()) {
       if (key === "agree") {
-        return; // Skip this iteration
+        continue;
       }
       if (key === "resume") {
-        sendResume(value);
-        return;
+        // error handle somewhere
+        resumeResult = await sendResume(value);
+        data[key] = resumeResult;
+        continue;
       }
       if (key === "shareEmail" || key === "mediaConsent") {
         data[key] = value === "on";
       } else {
-        // Initialize key with an empty string if it doesn't exist
-        if (!data[key]) {
-          data[key] = "";
-        }
-        data[key] += value + " ";
+        data[key] += value;
       }
-    });
+    }
 
     console.log(data);
 
@@ -42,5 +53,38 @@ export async function POST(request) {
   }
 }
 
-// Dummy function to handle resume
-function sendResume(resume) {}
+// Function to handle resume upload
+const sendResume = async (file) => {
+  if (!file) {
+    //empty, pass
+    return;
+  }
+
+  const ext = file.name.split(".").pop();
+  const uid = uuidv4().replace(/-/g, "");
+  const fileName = `${uid}${ext ? "." + ext : ""}`;
+
+  try {
+    const upload = new Upload({
+      client: s3,
+      params: {
+        Bucket,
+        Key: fileName,
+        Body: file,
+        ContentType: file.type,
+      },
+    });
+
+    // Monitor progress
+    upload.on("httpUploadProgress", (progress) => {
+      //console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+    });
+
+    await upload.done();
+    console.log(`File uploaded successfully as ${fileName}`);
+    return uid;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
