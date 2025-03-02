6070ea24e2521be729fe880b4ce7be2a0d8ffa1a
darshp623 <darshp717@gmail.com>
2025-03-02 17:36:01 -0500

    maybe fix sponsors?
    title

--- src/app/home_subpages/sponsors.js
+++ src/app/home_subpages/sponsors.js
@@ -15,6 +15,21 @@ export default function Sponsors() {
         });
     }, []);
 
+    function SponsorTier({ title, iconSrc, children }) {
+        return (
+            <div className="sponsors-tier-container" data-aos="fade-up">
+                <h1 className="sponsors-tier-title">
+                    <img src={iconSrc} alt="icon" className="sponsors-icon" />
+                    {title}
+                    <img src={iconSrc} alt="icon" className="sponsors-icon" />
+                </h1>
+                <div className="sponsors-logos">
+                    {children}
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="sponsors-page relative" style={{
             backgroundImage: "url('/hackumbc_bg.png')",
@@ -29,17 +44,15 @@ export default function Sponsors() {
                 Thank You to Our Incredible Sponsors!
             </div>
             <div className="sponsors-content relative z-10" >
-                <div className="sponsors-unified-container" data-aos="fade-up">
-                    <div className="sponsors-intro-text" data-aos="fade-up">
-                        hackUMBC wouldn't be possible without the generous support of our sponsors, click on their logos to check them out!
-                    </div>
-                    <div className="sponsors-all-logos" data-aos="fade-up">
+                <div className="sponsors-tiers">
+                    <SponsorTier title="OUR AMAZING SPONSORS" iconSrc="/partners.png" data-aos="fade-up">
                         {/* Diamond */}
                         <a href="https://coeit.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link diamond">
                             <img
                                 src="/coeit.png"
-                                alt="COEIT"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         
@@ -47,8 +60,9 @@ export default function Sponsors() {
                         <a href="https://www.troweprice.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link platinum">
                             <img
                                 src="/trowepricelogo.jpg"
-                                alt="T. Rowe Price"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         
@@ -56,8 +70,9 @@ export default function Sponsors() {
                         <a href="https://nightwing.us/" target="_blank" rel="noopener noreferrer" className="sponsor-link gold">
                             <img
                                 src="/nightwing.png"
-                                alt="Nightwing"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         
@@ -65,29 +80,33 @@ export default function Sponsors() {
                         <a href="https://www.jhuapl.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
                             <img
                                 src="/apl.png"
-                                alt="Johns Hopkins APL"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://www.ertcorp.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
                             <img
                                 src="/ert.jpg"
-                                alt="ERT"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://entrepreneurship.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
                             <img
                                 src="/alexbrowncenter.png"
-                                alt="Alex Brown Center"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://boozallen.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
                             <img
                                 src="/BAH.png"
-                                alt="Booz Allen Hamilton"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         
@@ -95,22 +114,25 @@ export default function Sponsors() {
                         <a href="https://cwit.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link bronze">
                             <img
                                 src="/CWIT.jpg"
-                                alt="CWIT"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://csee.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link bronze">
                             <img
                                 src="/CSEE.png"
-                                alt="CSEE"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://www.bestgateeng.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link bronze">
                             <img
                                 src="/bestgate.png"
-                                alt="Bestgate"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         
@@ -118,25 +140,28 @@ export default function Sponsors() {
                         <a href="https://standoutstickers.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link partner">
                             <img
                                 src="/stand-out-stickers.png"
-                                alt="Stand Out Stickers"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://mlh.io/" target="_blank" rel="noopener noreferrer" className="sponsor-link partner">
                             <img
                                 src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-white.png"
-                                alt="MLH"
+                                alt="sponsor"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
                         <a href="https://northropgrumman.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link partner">
                             <img
                                 src="/northrop.png"
-                                alt="Northrop Grumman"
+                                alt="sponsors"
                                 className="sponsors-logo"
+                                data-aos="fade-up"
                             />
                         </a>
-                    </div>
+                    </SponsorTier>
                 </div>
             </div>
         </div>
