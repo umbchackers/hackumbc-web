72bf612118248c334014a3039cdc426f54b9e55a
darshp623 <darshp717@gmail.com>
2025-03-02 17:10:08 -0500

    updated sponsors section
    title

--- src/app/home_subpages/sponsors.js
+++ src/app/home_subpages/sponsors.js
@@ -15,21 +15,6 @@ export default function Sponsors() {
         });
     }, []);
 
-    function SponsorTier({ title, iconSrc, children }) {
-        return (
-            <div className="sponsors-tier-container" data-aos= "fade-up">
-                <h1 className="sponsors-tier-title">
-                    <img src={iconSrc} alt="icon" className="sponsors-icon" />
-                    {title}
-                    <img src={iconSrc} alt="icon" className="sponsors-icon" />
-                </h1>
-                <div className="sponsors-logos">
-                    {children}
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div className="sponsors-page relative" style={{
             backgroundImage: "url('/hackumbc_bg.png')",
@@ -37,131 +22,121 @@ export default function Sponsors() {
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat"
         }}>
-            <div className="sponsors-section-title relative z-10 text-black" data-aos= "fade-up">
+            <div className="sponsors-section-title relative z-10 text-black" data-aos="fade-up">
                 <SectionTitle title="SPONSORS" />
             </div>
             <div className="sponsors-title-subheading relative z-10 text-black" data-aos="fade-up">
-                hackUMBC 2024 Sponsors
+                Thank You to Our Incredible Sponsors!
             </div>
             <div className="sponsors-content relative z-10" >
-                <div className="sponsors-tiers">
-                    <SponsorTier title="DIAMOND" iconSrc="/diamond.png" data-aos= "fade-up">
-                    <a href= "https://coeit.umbc.edu/" target="_blank" rel="noopener noreference">
-                        <img
-                            src="/coeit.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos="fade-up"
-                            href=""
-                        />
-                    </a>
-                    </SponsorTier>
-                    <SponsorTier title="PLATINUM" iconSrc="/platinum.png" data-aos= "fade-up">
-                    <a href= "https://www.troweprice.com/" target="_blank" rel="noopener noreference">
-                        <img
-                            src="/trowepricelogo.jpg"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    </SponsorTier>
-                    <SponsorTier title="GOLD" iconSrc="/gold.png" data-aos= "fade-up">
-                    <a href="https://nightwing.us/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/nightwing.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    </SponsorTier>
-                    <SponsorTier title="SILVER" iconSrc="/silver.png" data-aos= "fade-up">
-                    <a href="https://www.jhuapl.edu/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/apl.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://www.ertcorp.com/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/ert.jpg"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://entrepreneurship.umbc.edu/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/alexbrowncenter.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://boozallen.com/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/BAH.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    </SponsorTier>
-                    <SponsorTier title="BRONZE" iconSrc="/bronze.png" data-aos= "fade-up">
-                    <a href="https://cwit.umbc.edu/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/CWIT.jpg"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://csee.umbc.edu/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/CSEE.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://www.bestgateeng.com/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/bestgate.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    </SponsorTier>
-                    <SponsorTier title="PARTNERS" iconSrc="/partners.png" data-aos= "fade-up">
-                    <a href="https://standoutstickers.com/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/stand-out-stickers.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://mlh.io/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-white.png"
-                            alt="sponsor"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    <a href="https://northropgrumman.com/" target="_blank" rel="noopener noreferrer">
-                        <img
-                            src="/northrop.png"
-                            alt="sponsors"
-                            className="sponsors-logo"
-                            data-aos= "fade-up"
-                        />
-                    </a>
-                    </SponsorTier>
+                <div className="sponsors-unified-container" data-aos="fade-up">
+                    <div className="sponsors-intro-text" data-aos="fade-up">
+                        hackUMBC wouldn't be possible without the generous support of our sponsors, click on their logos to check them out!
+                    </div>
+                    <div className="sponsors-all-logos" data-aos="fade-up">
+                        {/* Diamond */}
+                        <a href="https://coeit.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link diamond">
+                            <img
+                                src="/coeit.png"
+                                alt="COEIT"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        
+                        {/* Platinum */}
+                        <a href="https://www.troweprice.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link platinum">
+                            <img
+                                src="/trowepricelogo.jpg"
+                                alt="T. Rowe Price"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        
+                        {/* Gold */}
+                        <a href="https://nightwing.us/" target="_blank" rel="noopener noreferrer" className="sponsor-link gold">
+                            <img
+                                src="/nightwing.png"
+                                alt="Nightwing"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        
+                        {/* Silver */}
+                        <a href="https://www.jhuapl.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
+                            <img
+                                src="/apl.png"
+                                alt="Johns Hopkins APL"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://www.ertcorp.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
+                            <img
+                                src="/ert.jpg"
+                                alt="ERT"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://entrepreneurship.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
+                            <img
+                                src="/alexbrowncenter.png"
+                                alt="Alex Brown Center"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://boozallen.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link silver">
+                            <img
+                                src="/BAH.png"
+                                alt="Booz Allen Hamilton"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        
+                        {/* Bronze */}
+                        <a href="https://cwit.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link bronze">
+                            <img
+                                src="/CWIT.jpg"
+                                alt="CWIT"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://csee.umbc.edu/" target="_blank" rel="noopener noreferrer" className="sponsor-link bronze">
+                            <img
+                                src="/CSEE.png"
+                                alt="CSEE"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://www.bestgateeng.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link bronze">
+                            <img
+                                src="/bestgate.png"
+                                alt="Bestgate"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        
+                        {/* Partners */}
+                        <a href="https://standoutstickers.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link partner">
+                            <img
+                                src="/stand-out-stickers.png"
+                                alt="Stand Out Stickers"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://mlh.io/" target="_blank" rel="noopener noreferrer" className="sponsor-link partner">
+                            <img
+                                src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-white.png"
+                                alt="MLH"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                        <a href="https://northropgrumman.com/" target="_blank" rel="noopener noreferrer" className="sponsor-link partner">
+                            <img
+                                src="/northrop.png"
+                                alt="Northrop Grumman"
+                                className="sponsors-logo"
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
