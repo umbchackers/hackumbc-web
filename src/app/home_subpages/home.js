9113a52d33b2e7debfecf8fb9f16957dc2640b35
darshp623 <darshp717@gmail.com>
2025-09-13 16:37:04 -0400

    update volunteer button

--- src/app/home_subpages/home.js
+++ src/app/home_subpages/home.js
@@ -78,9 +78,9 @@ export default function Home() {
             title="Devpost"
           />
           <LinkBox
-            href="https://hackumbc.typeform.com/to/c7JqYV9s"
-            desc="Want to be part of the behind-the-scenes magic? Click this button to sign up for our volunteer team!"
-            title="Volunteer"
+            href="#sponsors"
+            desc="Want to sponsor hackUMBC? Click this button to learn more!"
+            title="Sponsor"
           />
         </div>
       </div>
