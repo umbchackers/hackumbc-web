3c00e76774c3d17a343e8018053f78e13601bb08
JulianChavez <jchavez3@umbc.edu>
2025-09-27 02:58:58 -0400

    CHANGED SIGN-UP TEXT

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -165,7 +165,7 @@ export default function Survey() {
             </div>
 
             <h2 className="text-2xl font-bold mb-6 text-center text-white">
-              Sign Ups Open Now!
+              Sign Ups Are CLOSED!
             </h2>
             <p className="text-sm italic mb-6 text-center light-mode-text">
               Fields marked by <span className="text-red-500">*</span> are
