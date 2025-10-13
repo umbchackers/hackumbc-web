313371fd74f61f298eb37142d02c71b9ec1d0de0
darshp623 <darshp717@gmail.com>
2025-10-13 13:36:28 -0400

    added organizer application form for hackUMBC 2026

--- src/app/home_subpages/home.js
+++ src/app/home_subpages/home.js
@@ -67,11 +67,11 @@ export default function Home() {
           position: isMobile ? "relative" : "absolute",
           bottom: isMobile ? "auto" : "100px",
         }}>
-          {/* {<LinkBox
-            href="/sign-up"
-            desc="Registration for hackUMBC 2025 is now open! Click this button to go to the sign up page!"
-            title="Registration"
-          />} */}
+          <LinkBox
+            href="https://hackumbc.typeform.com/to/qM9YbL6E"
+            desc="Apply to be an Organizer for hackUMBC 2026! Applications are open until 10/25."
+            title="Apply to Organize"
+          />
           <LinkBox
             href="https://hackumbc-2025.devpost.com/"
             desc="Visit hackUMBC's Devpost to see project logistics, teams, and prizes!"
