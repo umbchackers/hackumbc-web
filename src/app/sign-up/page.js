6c5473d2f4f308b9c562fc5863f1595165ec2570
Hero Emenalom <heroemenalom73@gmail.com>
2026-02-20 12:59:37 -0500

    add previous-hackathons attended to form

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -737,6 +737,24 @@ export default function Survey() {
                   <option value="xxl">XXL</option>
                 </select>
               </div>
+              {/* How many hackathons */}
+              <div className="mb-4">
+                <label
+                  className="block text-white text-sm font-bold mb-2"
+                  htmlFor="previousHackathonsAttended"
+                >
+                  How many previous hackathons have you attended?{" "}
+                  <span className="text-red-500">*</span>
+                </label>
+                <input
+                  className="w-full px-3 py-2 bg-gray-800 text-white border border-gray-600 rounded-lg focus:outline-none focus:border-blue-500"
+                  type="number"
+                  id="previousHackathonsAttended"
+                  defaultValue={0}
+                  placeholder="# of hackathons attended"
+                  required
+                />
+              </div>
 
               {/* Dietary Restrictions */}
               <div className="mb-4">
