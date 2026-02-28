61fcaac3c4b6467a9cde13533a1969f3760b682b
bgoltser <igoltse1@umbc.edu>
2026-02-28 18:46:28 -0500

    updated email confirmation for mini hackathon

--- src/app/api/submit/EmailTemplate.tsx
+++ src/app/api/submit/EmailTemplate.tsx
@@ -16,31 +16,45 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     <p>Hi {firstName} {lastName},</p>
 
     <p>
-      You’re officially signed up for <b>hackUMBC 2025</b>, and we can't wait to
-      see you for 24&nbsp;hours of building, learning, and fun this fall at UMBC!
+      You’re officially signed up for the <b>hackUMBC Mini Hackathon</b>! We're excited to see what you 
+      build during this high-energy, 12-hour sprint!
     </p>
+
+    {/* theme & rules */}
+    <div style={{ border: '1px solid #ddd', padding: '10px', borderRadius: '5px', backgroundColor: '#fcfcfc' }}>
+      <p style={{ margin: 0 }}><b>💡 Theme:</b> Open-Ended</p>
+      <p style={{ margin: '5px 0 0 0' }}><b>👥 Team Size:</b> Maximum of 3 people</p>
+    </div>
 
     {/* event details */}
     <h2>📅 Event Details</h2>
     <ul>
       <li>
-        <b>🗓 Date:</b> September&nbsp;27-28, 2025
+        <b>🗓 Date:</b> April&nbsp;18th, 2026
       </li>
       <li>
-        <b>⏰ Check-In:</b> Saturday, September 27 at 9:00 AM - Engineering
-        Atrium
+        <b>⏰ Check-In:</b> Saturday, April 18th at 7:30 AM - ITE
+        2nd Floor
       </li>
       <li>
-        <b>📍 Location:</b> UMBC Engineering Building, 1000 Hilltop Circle,
+        <b>📍 Location:</b> UMBC Information Technology Engineering (ITE) Building, 1000 Hilltop Circle,
         Baltimore, MD 21250
       </li>
+    </ul>
+
+    {/* workshops & prizes */}
+    <h2>🏆 Why Attend?</h2>
+    <p>We have an incredible lineup for this mini hackathon, including:</p>
+    <ul>
+       <li><b>Workshops:</b> Technical sessions hosted by <b>GDG</b> and <b>CWIT Cyber Leads</b>.</li>
+       <li><b>Big Prizes:</b> Win a <b>Nintendo Switch 2</b> (1st), <b>Holy Stone Drones</b> (2nd), or <b>Amazon Fire Sticks 4K</b> (3rd)!</li>
     </ul>
 
     {/* discord */}
     <h2>🎧 Join the Discord</h2>
     <p>
       Connect with fellow participants, share ideas, and get updates by joining
-      the <b>hackUMBC Fall 2025</b> Discord server. Make sure to follow the
+      the hackUMBC Community Discord server. Make sure to follow the
       instructions in the <i>#rules-and-roles</i> channel after joining!
     </p>
     <p>
@@ -69,11 +83,10 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     /> */}
 
     {/* closing */}
-    <h2>🎉 We Can't Wait to See You!</h2>
+    <h2>🎉 We can't wait to see you!</h2>
     <p>
-      As the event gets closer, we'll send another email with details on what to
-      bring, parking information, the full schedule, and the free meals you'll
-      get throughout the weekend.
+      As the event gets closer, we'll send another email with details on parking information, the full schedule, 
+      and the free meals you'll get throughout the day.
     </p>
 
     <p>
