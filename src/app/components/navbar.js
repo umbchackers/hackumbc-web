544830ba25a3a1a5d760b1aa25e982d827b3cecd
Shakib Chowdhury <138811146+Swirly0@users.noreply.github.com>
2026-07-26 10:48:29 -0400

    fix 404 logo

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -54,7 +54,7 @@ export default function Navbar() {
                 }
               }}
             >
-              <img className="nav-logo light-mode-text" src="/hackumbc 2026 logo.svg" alt="logo" />
+              <img className="nav-logo light-mode-text" src="/hackumbc2026logo.svg" alt="logo" />
             </Link>
             <Link
               id="left-nav-button"
